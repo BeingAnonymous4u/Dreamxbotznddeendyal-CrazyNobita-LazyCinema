a2a8bc215a37ff55733d187b5bc3e0c3a1640505
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2026-09-26 09:14:34 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -255,7 +255,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """"<b><a href="https://t.me/+vkjRJwmDizo3MGI1">{file_name}</a></b>"""
+    CAPTION = """<b>📁 <a href="https://t.me/+vkjRJwmDizo3MGI1">{file_name}</a></b>"""
     
     MOVIE_UPDATE_NOTIFY_TXT = """
 </b><a href={poster_url}>📥</a><a href={imdb_url}>New {tag} Added</a></b>
